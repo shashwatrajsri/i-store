727f0b417d8613939981aac075f1f515f2057a3f
shashwatrajsri <ss4521811@gmail.com>
2021-12-05 21:49:44 +0530

    initial commit

---
+++ src/actions/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+export const loginUser = (email, password) => {
+    // VERIFY EMAIL AND PASSWORD
+    // create and sign a JWT
+    const users = JSON.parse(localStorage.getItem('users')) ?? []
+    const user = users.find(u => u.email === email)
+
+    if (user.password === password) {
+        const token = jwt.sign({ email: user.email }, 'SECRET')
+        return {
+            type: "LOGIN_SUCCESS",
+            payload: { token }
+        }
+    } else {
+        window.alert("INCORRECT CREDS")
+        return {
+            type: "LOGIN_FAILED",
+            payload: { token: null }
+        }
+    }
+}
